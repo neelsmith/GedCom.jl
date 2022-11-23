7eb724bb494df337974382c679eda31a6198202c
Neel Smith <dnsmith.neel@gmail.com>
2022-11-23 04:13:03 -0500

    Add docstrings

--- src/valuegraph.jl
+++ src/valuegraph.jl
@@ -6,6 +6,12 @@ function idindex(id, people)
     findfirst(p -> p[:id] == id, people)
 end
 
+
+"""Create a value graph for `gen`.
+Nodes have the ID and label for individuals,
+edges have a string with one of three relationship types:
+"husband", "wife" or "child".
+"""
 function genealogyGraph(gen::Genealogy)
     folks = map(gen.individuals) do i
         (id = i.id, name = label(i))
@@ -26,7 +32,6 @@ function genealogyGraph(gen::Genealogy)
         w = wifeid(f)
         h_index = 0
         w_index = 0
-
      
         if isempty(h) || isempty(w)
         else
@@ -49,45 +54,4 @@ function genealogyGraph(gen::Genealogy)
         
     end
     g
-
 end
-
-#=
-"""
-swissmetro_graph()
-
-A small example graph for using in documentation.
-
-Swissmetro was a planned (but never realised) Hyperloop style project in Switzerland.
-All data was taken from Wikipedia: https://en.wikipedia.org/wiki/Swissmetro
-"""
-function swissmetro_graph()
-
-cities = [
-    (name = "Basel", population = 117_595),
-    (name = "Bern", population = 133_791),
-    (name = "Genève", population = 201_818),
-    (name = "Lausanne", population = 139_111),
-    (name = "St. Gallen", population = 75_833),
-    (name = "Zürich", population = 415_215),
-]
-
-
-
-g = ValGraph{Int8}(
-    6;
-    vertexval_types=(name = String, population = Int32),
-    vertexval_init=v -> cities[v],
-    edgeval_types=(distance=Float64,)
-)
-
-add_edge!(g, 1, 6, (distance=89.0,))
-add_edge!(g, 2, 4, (distance=81.0,))
-add_edge!(g, 2, 6, (distance=104.0,))
-add_edge!(g, 3, 4, (distance=68.0,))
-add_edge!(g, 5, 6, (distance=69.0,))
-
-return g
-end
-=#
-
