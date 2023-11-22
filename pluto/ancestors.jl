c236f07030f70849daa21a2545c9e137b257d487
Neel Smith <dnsmith.neel@gmail.com>
2023-11-22 13:15:05 -0500

    Save nb state

--- pluto/ancestors.jl
+++ pluto/ancestors.jl
@@ -22,37 +22,30 @@ begin
 	using GedCom
 	Pkg.add("PlutoUI")
 	using PlutoUI
+	Pkg.add("PlutoTeachingTools")
+	using PlutoTeachingTools
+
+
+	danger(md"""
+The organization of this notebook's environment in this cell is  a hack until `GedCom.jl` is  published on juliahub.
+""")
 end
 
 # ╔═╡ eb702a7e-6a96-47b9-aeb2-46eac03dc561
 md"""
-# `GedCom.jl`: ancestry trees
-"""
-
-# ╔═╡ 06e9ba77-502a-45d8-9f95-11129db03606
-md"""
-> The organization of this notebook's environment (in the following cell) is  a hack until `GedCom.jl` is  published on juliahub.
+# Explore GEDCOM data with `GedCom.jl`
 """
 
 # ╔═╡ bde8a9a3-71b1-4d0e-8e54-c0616fbe7c3e
-md"""
+tip(md"""
 ## TBD
 
 - add type-ahead filtering of names
 
-"""
-
-# ╔═╡ 53668273-179a-4596-97fa-24db84556236
-md"""## Ancestry trees"""
+""")
 
 # ╔═╡ 0d6ac6de-cfee-4b25-ac2f-9ae06ce4ecd6
 md"*Use demo file of Presidents?* $(@bind usedemo CheckBox(default=true))"
-
-# ╔═╡ 1e7d7661-44aa-468a-af3d-c25864bfd9c1
-html"""<br/><br/><br/><br/><br/>"""
-
-# ╔═╡ baa3fdf4-b780-406e-bfd5-e5c7e4dc3552
-md"""> Data loading and setup"""
 
 # ╔═╡ ca028862-5d01-11ed-0e13-01f6b07abf83
 f = if usedemo
@@ -60,6 +53,18 @@ f = if usedemo
 else
 	@bind f FilePicker()
 end
+
+# ╔═╡ 53668273-179a-4596-97fa-24db84556236
+md"""## Ancestors"""
+
+# ╔═╡ 46004ab9-ac76-4817-a4af-54c63c404626
+md"""## Descendants"""
+
+# ╔═╡ 1e7d7661-44aa-468a-af3d-c25864bfd9c1
+html"""<br/><br/><br/><br/><br/>"""
+
+# ╔═╡ baa3fdf4-b780-406e-bfd5-e5c7e4dc3552
+md"""> Data loading and setup"""
 
 # ╔═╡ be67734a-90a6-4220-926c-39c1d0e89030
 # ╠═╡ show_logs = false
@@ -114,27 +119,19 @@ end
 # ╔═╡ eca70583-735f-4a83-a304-cd9d63a53834
 prnts = isnothing(gen) ? [] : GedCom.parents(person, gen)
 
-# ╔═╡ a4213897-b989-4e4d-8ec2-fc73959af7f7
-prnts.father
-
-# ╔═╡ 986af820-0a18-4751-809f-920090181197
-prnts.mother
-
 # ╔═╡ Cell order:
-# ╟─eb702a7e-6a96-47b9-aeb2-46eac03dc561
-# ╟─06e9ba77-502a-45d8-9f95-11129db03606
 # ╟─bd080239-bf0e-4cfd-8126-aec87a29b908
+# ╟─eb702a7e-6a96-47b9-aeb2-46eac03dc561
 # ╟─bde8a9a3-71b1-4d0e-8e54-c0616fbe7c3e
-# ╟─53668273-179a-4596-97fa-24db84556236
 # ╟─0d6ac6de-cfee-4b25-ac2f-9ae06ce4ecd6
+# ╟─ca028862-5d01-11ed-0e13-01f6b07abf83
+# ╟─53668273-179a-4596-97fa-24db84556236
 # ╟─d79deaf9-b0e7-4d48-bf8b-4f823848e7d9
 # ╟─1e63fe6d-be8c-43b6-847f-0dc73f3d5657
 # ╠═eca70583-735f-4a83-a304-cd9d63a53834
-# ╠═a4213897-b989-4e4d-8ec2-fc73959af7f7
-# ╠═986af820-0a18-4751-809f-920090181197
+# ╟─46004ab9-ac76-4817-a4af-54c63c404626
 # ╟─1e7d7661-44aa-468a-af3d-c25864bfd9c1
 # ╟─baa3fdf4-b780-406e-bfd5-e5c7e4dc3552
-# ╟─ca028862-5d01-11ed-0e13-01f6b07abf83
 # ╟─be67734a-90a6-4220-926c-39c1d0e89030
 # ╟─f9aeeb3a-bfc2-4a15-8f8d-3dae415a4f11
 # ╟─ca2f6e6e-094f-4a98-a568-2dc858d3fda0
