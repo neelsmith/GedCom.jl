3adef3e71947fdf060fa20dfa07f610b535e34c5
Neel Smith <dnsmith.neel@gmail.com>
2023-11-25 09:02:37 -0500

    Add placeholder for siblings display

--- pluto/ancestors.jl
+++ pluto/ancestors.jl
@@ -66,14 +66,14 @@ end
 # ╔═╡ 53668273-179a-4596-97fa-24db84556236
 md"""## Individual"""
 
-# ╔═╡ cef20de7-3e9c-4683-b925-73a56c5e7b09
-md"""### Marriages"""
-
 # ╔═╡ 87ae5c59-cd8e-42d0-8956-6a29efd7678f
 md"""### Ancestors"""
 
 # ╔═╡ b90dd261-5ba5-4fcc-b83f-8babdca51ff4
 md"""`GedCom.parents` returns a named tuple with mother and father:"""
+
+# ╔═╡ cef20de7-3e9c-4683-b925-73a56c5e7b09
+md"""### Marriages"""
 
 # ╔═╡ 46004ab9-ac76-4817-a4af-54c63c404626
 md"""### Descendants"""
@@ -117,14 +117,6 @@ md"""
 """
 
 
-# ╔═╡ 4b91b668-e568-4c23-ac2b-049812851e1a
-if isnothing(gen) 
-	md""
-else
-	items = map(mrg -> string("- ", label(mrg)), GedCom.nuclearfamilies(person, gen)) 
-	join(items,"\n") |> Markdown.parse
-end
-
 # ╔═╡ 1e63fe6d-be8c-43b6-847f-0dc73f3d5657
 if isnothing(gen)
 	md""
@@ -148,11 +140,22 @@ else
 	end
 end
 
+# ╔═╡ c281779b-1576-43cd-9610-bd0aac85f3dc
+isnothing(gen) ? nothing : GedCom.siblings(person, gen)
+
 # ╔═╡ 1f999199-70c6-4922-a231-0a260d6cc672
 isnothing(gen) ? md"" : md"""**Ancestor tree** for *$(GedCom.label(person))*"""
 
 # ╔═╡ 49ab89e8-a10f-432a-88b5-d4f8b3b3a1d4
 isnothing(gen) ? md"" : GedCom.ancestor_tree_md(person, gen) |> Markdown.parse
+
+# ╔═╡ 4b91b668-e568-4c23-ac2b-049812851e1a
+if isnothing(gen) 
+	md""
+else
+	items = map(mrg -> string("- ", label(mrg)), GedCom.nuclearfamilies(person, gen)) 
+	join(items,"\n") |> Markdown.parse
+end
 
 # ╔═╡ eedb9128-4719-4213-9c91-81871494a0de
 isnothing(gen) ? md"" : md"""**Descendant tree** for *$(GedCom.label(person))*"""
@@ -170,13 +173,14 @@ isnothing(gen) ? md"" : GedCom.descendant_tree_md(person, gen) |> Markdown.parse
 # ╟─2272b18a-c033-4779-a1fa-1920a48cf9b9
 # ╟─53668273-179a-4596-97fa-24db84556236
 # ╟─d79deaf9-b0e7-4d48-bf8b-4f823848e7d9
-# ╟─cef20de7-3e9c-4683-b925-73a56c5e7b09
-# ╟─4b91b668-e568-4c23-ac2b-049812851e1a
 # ╟─87ae5c59-cd8e-42d0-8956-6a29efd7678f
 # ╟─b90dd261-5ba5-4fcc-b83f-8babdca51ff4
 # ╟─1e63fe6d-be8c-43b6-847f-0dc73f3d5657
+# ╠═c281779b-1576-43cd-9610-bd0aac85f3dc
 # ╟─1f999199-70c6-4922-a231-0a260d6cc672
 # ╟─49ab89e8-a10f-432a-88b5-d4f8b3b3a1d4
+# ╟─cef20de7-3e9c-4683-b925-73a56c5e7b09
+# ╟─4b91b668-e568-4c23-ac2b-049812851e1a
 # ╟─46004ab9-ac76-4817-a4af-54c63c404626
 # ╟─eedb9128-4719-4213-9c91-81871494a0de
 # ╟─e9cc00c9-c058-43a7-988b-4d17e976aae0
