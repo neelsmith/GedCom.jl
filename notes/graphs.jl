ea3d46d0acd273998abc63b4f7b6e71289afaa46
Neel Smith <dnsmith.neel@gmail.com>
2024-01-02 06:04:28 -0500

    Experiment with graphs

--- notes/graphs.jl
+++ notes/graphs.jl
@@ -1,10 +1,27 @@
 using GedCom
+using Graphs
 
 f = joinpath(pwd(),"test","data","pres2020.ged")
 gen = genealogy_g5(f)
 
 gengraph = genealogyGraph(gen)
 
-using Plots
-using GraphRecipes
-graphplot(gengraph)
+##using Plots
+using Graphs
+using SimpleValueGraphs
+
+nameidx = map(vlist) do i
+    (v = i, name = get_vertexval(gr, i, :name))
+end
+
+
+using CairoMakie
+using GraphMakie
+GraphMakie.graphplot(gengraph)
+
+using SimpleValueGraphs
+get_vertexval(gengraph, 1200, :name)
+
+ValMatrix(gengraph, :name, 1200)
+
+get_vertexval(gengraph, 6, :)
