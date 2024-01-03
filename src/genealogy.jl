dd603d76a19dcbbd98ea7fef5c7aa9925c0df10c
Neel Smith <dnsmith.neel@gmail.com>
2024-01-02 19:38:39 -0500

    Count generations

--- src/genealogy.jl
+++ src/genealogy.jl
@@ -52,29 +52,28 @@ a given family unit.
 $(SIGNATURES)
 """
 function descendant_generations(gen::Genealogy, fam::FamilyUnit, count = 0)
-    @info("AT GENERATOIN $(count)")
-    @info("husband id " * husbandid(fam))
+    @debug("AT GENERATOIN $(count)")
+    @debug("husband id " * husbandid(fam))
     husband = individual( husbandid(fam), gen)
-    @info(label(husband))
-    @info("wife id " * wifeid(fam))
+    @debug(label(husband))
+    @debug("wife id " * wifeid(fam))
     wife = individual( wifeid(fam), gen)
-    @info(label(wife))
+    @debug(label(wife))
     kids = childrenids(fam)
-    @info("$(length(kids)) children ")
+    @debug("$(length(kids)) children ")
     if ! isempty(kids)
         count = count + 1
     end
-    @info("So now counting at $(count)")
+    @debug("So now counting at $(count)")
     for kid_id in kids
         kid = individual(kid_id, gen)
         nextmarriages =  family_ids_spouse(kid)
-        @info("Kid $(label(kid)): $(length(nextmarriages)) marriage(s)")
+        @debug("Kid $(label(kid)): $(length(nextmarriages)) marriage(s)")
 
         for mrg in nextmarriages 
             nextfam = family(mrg, gen)
             count = descendant_generations(gen, nextfam, count)
         end
-        #count = descendant_generations(gen, )
     end
     return count
 end
@@ -86,30 +85,30 @@ end
 $(SIGNATURES)
 """
 function ancestor_generations(gen::Genealogy, indi::Individual, count = 0)
-    @info("AT GENERATOIN $(count)")
-    @info("individual " * label(indi))
+    @debug("AT GENERATOIN $(count)")
+    @debug("individual " * label(indi))
     parenttuple = parents(indi, gen)
     rents = filter([parenttuple.father, parenttuple.mother]) do parent
         ! isnothing(parent)
     end
     if ! isempty(rents)
-        @info("Bumping gneeration count becase $(label(indi)) has parents $(label.(rents))")
+        @debug("Bumping gneeration count becase $(label(indi)) has parents $(label.(rents))")
         count = count + 1
     end
 
 
     fathercount = if isnothing(parenttuple.father) 
-        @info("Buit parenttuple.father is nothing!")
+        @debug("Buit parenttuple.father is nothing!")
         count
     else
         newfather = ancestor_generations(gen, parenttuple.father, count)
-        @info("Got new father count $(newfather)")
+        @debug("Got new father count $(newfather)")
         newfather
     end
     mothercount = isnothing(parenttuple.mother) ? count : ancestor_generations(gen, parenttuple.mother, count)
-    @info("Get max of $(fathercount), $(mothercount)")
+    @debug("Get max of $(fathercount), $(mothercount)")
     count = maximum([fathercount, mothercount])
-    @info("Returning $(count)")
+    @debug("Returning $(count)")
 
     return count
 
