08c1167f0fe0820443e2276bc50d9a2653004f33
dnsmith.neel@gmail.com <dnsmith.neel@gmail.com>
2025-04-25 11:24:35 -0400

    Graphs notes

--- notes/graphs.jl
+++ notes/graphs.jl
@@ -1,5 +1,6 @@
 using GedCom
 using Graphs
+using SimpleValueGraphs
 
 f = joinpath(pwd(),"test","data","pres2020.ged")
 gen = genealogy_g5(f)
@@ -11,18 +12,21 @@ using SimpleValueGraphs
 
 vlist = vertices(gengraph) |> collect
 
+# Pair up in a named tuple the ID and a readable label for 
+# every node in the graph:
 nameidx = map(vlist) do i
     (v = i, name = get_vertexval(gengraph, i, :name))
 end
 
 
+# Learn how to use value graphs:
+get_vertexval(gengraph, 1200, :name)
+ValMatrix(gengraph, :name, 1200)
+get_vertexval(gengraph, 6, :)
+
+
+
+# Plotting with defaults
 using CairoMakie
 using GraphMakie
 GraphMakie.graphplot(gengraph)
-
-using SimpleValueGraphs
-get_vertexval(gengraph, 1200, :name)
-
-ValMatrix(gengraph, :name, 1200)
-
-get_vertexval(gengraph, 6, :)
